c6a1bb321b0001c154d17bc217d5a3bbd637f958
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-09 18:08:25 -0400

    update lib

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+# Hello, programmer!
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Mohamed") 
+# Hello, Mohamed!
+
+greet("Mohamed")
+# Hello, Mohamed!
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, " + name + "!")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
